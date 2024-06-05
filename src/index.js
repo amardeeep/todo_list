7414df6b53f405b355187a786ad58ffdebd6be97
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-05 18:23:34 +0530

    finish making projects to create project and store todos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 //import "./styleIndex.css";
 import { todo } from "./todo.js";
-console.log(todo("title", "desc", "date", "prio"));
+let todo1 = todo("title", "desc", "date", "prio");
+//create function that creates projects object having name and todos
+function projects(name) {
+  let todos = [];
+  function addTodo(todo) {
+    todos.push(todo);
+  }
+  return {
+    addTodo,
+    name,
+    todos,
+  };
+}
+let projectDefault = projects("Default", todo1);
+projectDefault.addTodo(todo("ti", "kk", "ll", "mm"));
+console.log(projectDefault);
--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,20 @@
-function todo(title, description = null, dewdate, priority) {
+function todo(title, description = null, dewdate, priorityInput) {
+  let priority = priorityInput;
+  function changePriority(newPriority) {
+    priority = newPriority;
+  }
+  let isComplete = false;
+  function markComplete() {
+    isComplete = true;
+  }
   return {
+    changePriority,
     title,
     description,
     dewdate,
     priority,
+    isComplete,
+    markComplete,
   };
 }
 export { todo };
