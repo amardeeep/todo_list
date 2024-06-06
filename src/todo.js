34e412c114a012e54ebe41fc3bc434ccb7d92c6f
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-06 09:21:30 +0530

    fixed todo function and create separate project

--- src/todo.js
+++ src/todo.js
@@ -1,19 +1,47 @@
 function todo(title, description = null, dewdate, priorityInput) {
-  let priority = priorityInput;
-  function changePriority(newPriority) {
-    priority = newPriority;
-  }
+  //function to toggle betWeen isComplete is true and false
   let isComplete = false;
   function markComplete() {
-    isComplete = true;
+    if (!isComplete) {
+      isComplete = true;
+    } else if (isComplete) {
+      isComplete = false;
+    }
   }
-  return {
-    changePriority,
+
+  let todoObject = {
     title,
     description,
     dewdate,
-    priority,
+    priorityInput,
     isComplete,
+  };
+
+  //function to edit details of a todo
+  function editToDo(todoObject) {
+    function changeTitle(todoObject, change) {
+      todoObject.title = change;
+    }
+    function changeDesciption(todoObject, change) {
+      todoObject.description = change;
+    }
+    function changeDewDate(todoObject, change) {
+      todoObject.dewdate = change;
+    }
+    function changePriority(todoObject, change) {
+      todoObject.priority = change;
+    }
+    return {
+      todoObject,
+      changeDesciption,
+      changeTitle,
+      changePriority,
+      changeDewDate,
+    };
+  }
+  return {
+    todoObject,
+    editToDo,
     markComplete,
   };
 }
